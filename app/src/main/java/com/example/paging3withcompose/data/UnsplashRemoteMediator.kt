00f99db9bcdbcdc9a16433fc7563977ece0cb0e4
AngryMause <chostblade@gmail.com>
2022-10-14 17:27:18 +0300

    add remoteMediator

--- app/src/main/java/com/example/paging3withcompose/data/UnsplashRemoteMediator.kt
+++ app/src/main/java/com/example/paging3withcompose/data/UnsplashRemoteMediator.kt
@@ -1,4 +1,29 @@
 package com.example.paging3withcompose.data
 
-class UnsplashRemoteMediator {
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.example.paging3withcompose.data.local.UnsplashImageDB
+import com.example.paging3withcompose.data.remote.UnsplashApi
+import com.example.paging3withcompose.model.UnsplashImage
+import javax.inject.Inject
+
+@OptIn(ExperimentalPagingApi::class)
+class UnsplashRemoteMediator @Inject constructor(
+    private val unsplashApi: UnsplashApi,
+    private val unsplashImageDB: UnsplashImageDB,
+) : RemoteMediator<Int, UnsplashImage>() {
+    private val unsplashImageDao = unsplashImageDB.unsplashImage()
+    private val unsplashRemoteKey = unsplashImageDB.unsplashRemoteKeys()
+
+
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, UnsplashImage>,
+    ): MediatorResult {
+        TODO()
+    }
+
+
 }
